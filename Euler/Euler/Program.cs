c752cd7a00c31f364b44658c9a6db9a007f59bc0
ShadowScreamz <31037065+ShadowScreamz@users.noreply.github.com>
2019-02-15 16:36:21 +0200

    Update Program.cs

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -10,34 +10,7 @@ namespace Euler
     {
         static void Main(string[] args)
         {
-            //created by ShadowScreamz
-            //Prac 1 Task 3 Euler
-            //solution to Euler Problem 3: Largest Prime Factor
-
-            long number = 600851475143; 
-            long largestprimefactor= 0;
-
-            for (long i = 2; i < number; i++)
-            {
-                if (number % i == 0)                //number is a prime
-                { 
-                  
-                    for (long j = 2; j < i; j++)
-                    {
-                        if (i % j == 0)             //number is not a prime
-                        {
-                            break;
-                        }
-                        else
-                        {
-                            largestprimefactor = i;
-                        }
-                        Console.WriteLine($"the largest prime factor for {number} is : {largestprimefactor}");
-                        Console.ReadLine();
-                    }
-                  
-                }
-            }
+          
         }
     }
 }
